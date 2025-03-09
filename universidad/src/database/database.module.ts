7daf27b2b90face6eb792fdaf770d6fc8e5b4603
stoic-programmer11 <schumaceroespada@gmail.com>
2025-03-09 15:36:43 -0400

    refactor: Clean up DatabaseModule imports and remove unused entities and services

--- universidad/src/database/database.module.ts
+++ universidad/src/database/database.module.ts
@@ -2,18 +2,15 @@ import { Module } from '@nestjs/common';
 import { DatabaseService } from './database.service';
 import { DatabaseController } from './database.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Tipos_ambientes } from '../entities/tipos_ambientes.entity';
-import { Universidades } from '../entities/universidades.entity';
-import { UniversidadesService } from '../services/universidades.service';
-import { UniversidadesController } from '../controllers/universidades.controller';
+
 
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([Tipos_ambientes, Universidades
+    TypeOrmModule.forFeature([
 ])],
   providers: [
-    DatabaseService, UniversidadesService, UniversidadesController, Tramites_documentosService, Tramites_documentosController, SexosService, SexosController,
+    DatabaseService,
 ],
   controllers: [DatabaseController],
   exports: [DatabaseService],
